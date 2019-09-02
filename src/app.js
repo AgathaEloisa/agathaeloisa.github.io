24fbc139f48c92dc091ec76ae9d76b382a243686
AgathaP <ebrumaktub@gmx.com>
2019-09-02 10:50:20 -0400

    responsive adaptado a dispositivos moviles

--- src/app.js
+++ src/app.js
@@ -2,8 +2,69 @@ import { skills } from './js/skills-data.js';
 import { proyects } from './js/proyects-data.js'
 import { contact } from './js/contact-data.js';
 
+
+document.getElementById('root').innerHTML = `
+        <!-- vista principal -->
+        <a name="inicio">
+            <section class="main">
+                <div class="title">
+                    <h1>Agatha Sáez</br>
+                        Desarrolladora Front-end</h1>
+                </div>
+                <div class="my-photo">
+                    <img src="assets/img/yo.jpg" alt="Foto de perfil" />
+                </div>
+            </section>
+        </a>
+
+        <!-- Vista about -->
+        <a name="about">
+            <section class="about_me">
+                <div class="about-text">
+                    <h2>Sobre mí</h2>
+                    <p> ipsum dolor sit ametl
+                        consectetur adipisicing elit. Ut id harum,
+                        totam deserunt molestias ipsum architecto
+                        amet nihil sit voluptatem labore temporibus
+                        libero saepe asperiores sapiente aperiam quos
+                        eum tempora? </p>
+                </div>
+            </section>
+        </a>
+
+        <!-- vista Skills -->
+        <a name="skills">
+            <section class="skills" id="skills">
+                <div class="skills-title">
+                    <h2>Habilidades técnicas</h2>
+                </div>
+
+            </section>
+        </a>
+
+        <!-- Vista Proyects -->
+        <a name="proyects">
+            <section class="proyects" id="proyects">
+                <div class="proyects-title">
+                    <h2>Mis Proyectos</h2>
+                </div>
+            </section>
+        </a>
+
+        <!-- Vidta contacto -->
+        <a name="contact">
+            <section class="contact_me" id="contact_me">
+                <div class="contact-title">
+                    <h2>Contáctame</h2>
+                </div> 
+
+
+            </section>
+
+        <footer> Nezu Project&reg; 🐀</footer>
+`
+
 // skills / itero la data e imprimo logos
-    console.log(skills)
     for(let i = 0; i < skills.length; i++){
         document.getElementById('skills').innerHTML += `
     <div class="skills-icons">
@@ -14,7 +75,6 @@ import { contact } from './js/contact-data.js';
 
 
 // Proyects / imprimo proyectos en una tarjeta, lo itero
-console.log(proyects[0].title)
 for(let i = 0; i < proyects.length; i++){
 document.getElementById('proyects').innerHTML += `
   <div class="card">
--- src/js/skills-data.js
+++ src/js/skills-data.js
@@ -4,6 +4,9 @@ export const skills = [
     { name:'JS', img: './assets/icon_skills/js.png' },
     { name: 'Git', img: './assets/icon_skills/git.png' },
     { name:'Github', img: './assets/icon_skills/github.png' },
-    { name: 'NodeJS', img: './assets/icon_skills/nodejs.png' },
+    { name: 'Node JS', img: './assets/icon_skills/nodejs.png' },
     { name:'Angular', img: './assets/icon_skills/angular.png' },
+    { name:'Firebase', img: './assets/icon_skills/firebase.png' },
+    { name:'Bootstrap', img: './assets/icon_skills/bootstrap.png' },
+    { name:'Materialize', img: './assets/icon_skills/materialize.png' },
 ]
