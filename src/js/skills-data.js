4d19b8a6edaec77d4514d32ca9c1a750158a973a
AgathaP <ebrumaktub@gmx.com>
2019-10-01 10:40:47 -0300

    cambio rutas de imagenes en datas respectivas y en html, y cambio de lugar html

--- src/js/skills-data.js
+++ src/js/skills-data.js
@@ -1,15 +1,15 @@
 export const skills = [
-    { name:'CSS', img: './assets/icon_skills/css.png', class: 'each_icon' },
-    { name: 'HTML', img: './assets/icon_skills/html.png', class: 'each_icon' },
-    { name:'JS', img: './assets/icon_skills/js.png', class: 'each_icon' },
-    { name: 'Git', img: './assets/icon_skills/git.png', class: 'each_icon' },
-    { name:'Github', img: './assets/icon_skills/github.png', class: 'diferent_icon' },
-    { name: 'Node JS', img: './assets/icon_skills/nodejs.png', class: 'each_icon' },
-    { name:'Angular', img: './assets/icon_skills/angular.png', class: 'each_icon' },
-    { name:'Firebase', img: './assets/icon_skills/firebase.png', class: 'each_icon' },
-    { name:'Bootstrap', img: './assets/icon_skills/bootstrap.png', class: 'diferent_icon' },
-    { name:'Materialize', img: './assets/icon_skills/materialize.png', class: 'each_icon' },
-    { name: 'Figma', img: './assets/icon_skills/figma.png', class: 'each_icon' }
+    { name:'CSS', img: 'src/assets/icon_skills/css.png', class: 'each_icon' },
+    { name: 'HTML', img: 'src/assets/icon_skills/html.png', class: 'each_icon' },
+    { name:'JS', img: 'src/assets/icon_skills/js.png', class: 'each_icon' },
+    { name: 'Git', img: 'src/assets/icon_skills/git.png', class: 'each_icon' },
+    { name:'Github', img: 'src/assets/icon_skills/github.png', class: 'diferent_icon' },
+    { name: 'Node JS', img: 'src/assets/icon_skills/nodejs.png', class: 'each_icon' },
+    { name:'Angular', img: 'src/assets/icon_skills/angular.png', class: 'each_icon' },
+    { name:'Firebase', img: 'src/assets/icon_skills/firebase.png', class: 'each_icon' },
+    { name:'Bootstrap', img: 'src/assets/icon_skills/bootstrap.png', class: 'diferent_icon' },
+    { name:'Materialize', img: 'src/assets/icon_skills/materialize.png', class: 'each_icon' },
+    { name: 'Figma', img: 'src/assets/icon_skills/figma.png', class: 'each_icon' }
 ]
 
 // export const skills = [
