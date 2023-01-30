379d7e5eedc0df166cbb0b2d8f1f55d13a36afc8
Agatha Eloísa <ebrumaktub@gmx.com>
2023-01-29 22:09:26 -0300

    actualizo información de contacto

--- src/js/contact-data.js
+++ src/js/contact-data.js
@@ -2,6 +2,6 @@
 
 export const contact = [
     { link: 'http://www.linkedin.com/in/agatha-sáez-v', alt: 'Linkedin', icon: 'fab fa-linkedin-in' },
-    { link: 'http://github.com/AgathaP', alt: 'Github', icon: 'fab fa-github-alt' },
+    { link: 'http://github.com/AgathaEloisa', alt: 'Github', icon: 'fab fa-github-alt' },
     { link: 'mailto:agatha.saez@outlook.cl', alt: 'agatha.saez@outlook.cl', icon: 'far fa-envelope' }
 ]
